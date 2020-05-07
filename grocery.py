f99a1a5f614f9c3aa53afbbaf57d67cbfbeeabac
treethan <ethanjamesberman@gmail.com>
2020-05-06 22:34:24 -0700

    Complete Working Version of Meal Shuffle

--- grocery.py
+++ grocery.py
@@ -3,20 +3,27 @@ import xlrd
 import sys
 import random
 from collections import defaultdict
+from random import randint
 
 xl = pd.ExcelFile("/Users/eberman/projects/meal_shuffle.xlsx")
 
+#opens workbook and specific worksheet
+workbook = xlrd.open_workbook("/Users/eberman/projects/meal_shuffle.xlsx")
+worksheet = workbook.sheet_by_name(workbook.sheet_names()[0])
+recipe_num = randint(1,15)
+
 def displayData():
 
-    #default list created via default library
+    #default list created via collections library
     result = defaultdict(list)
 
     #opens meal_shuffle data workbook
     workbook = xlrd.open_workbook("/Users/eberman/projects/meal_shuffle.xlsx")
-    #opens/reads the sheet on the workbook
+    #opens/reads the first sheet on the workbook
     worksheet = workbook.sheet_by_name(workbook.sheet_names()[0])
     #indicates the header row
     headers = worksheet.row(0)
+    #indicates index column
     index = worksheet.col(0)
 
     for index, header in enumerate(headers):
@@ -25,23 +32,8 @@ def displayData():
         else:
             print(str(index) + ". " + header.value)
 
-    # #in the first sheet of the workbook
-    # for index in range(worksheet.nrows)[1:]:
-    #     #iterate through headers/columns in worksheet
-    #     for header, col in zip(headers, worksheet.row(index)):
-    #         #appends header/column value
-    #         result[header.value].append(col.value)
-       
-    #formats data with pandas library
-    # df = xl.parse(xl.sheet_names[0])
-
-    # print (df)
 
 def pullRecipe():
-
-    #opens workbook and specific worksheet
-    workbook = xlrd.open_workbook("/Users/eberman/projects/meal_shuffle.xlsx")
-    worksheet = workbook.sheet_by_name(workbook.sheet_names()[0])
 
     #prompt user input for which recipe they would like to see
     d_recipe = input("\nPlease select a recipe you would like to view (1 to 15): ")
@@ -58,10 +50,54 @@ def pullRecipe():
         else:
             # prints ingredients (.value is needed here since we need the value of the cell)
             items = items.value
-            print(items)
+            if items == "":
+                pass
+            else:
+                print(items)
+                
+    print("\n")
 
-#def randomizeRecipe(pullRecipe):
+
+def randomizeRecipe():
+
+    active = True
+    while active:
+        prompt = input("Let's have some fun! Since nobody can choose what to have for dinner, let's roll the dice! ('r' to roll for a recipe, 'q' to quit): ")
+        if prompt == 'q':
+            active = False
+        elif prompt == 'r':
+            #same looping as in the pullRecipe method, but does not include user input and uses a randint to select a recipe
+            recipe_num = randint(1,15)
+            recipe = worksheet.col(int(recipe_num))
+            for items in recipe:
+            #ignores the first row in the spreadsheet which lists the recipe name
+                if items == recipe[0]:
+                    print(" ")
+                else:
+                    # prints ingredients (.value is needed here since we need the value of the cell)
+                    items = items.value
+                    if items == "":
+                        pass
+                    else:
+                        print(items)
+    print("\n")
+
+            
+            
+        
 
 
-displayData()
-pullRecipe()
+print("Welcome to Meal Shuffle! Would you like to view a recipe, or have one selected at random?: \n"
+      "1. Individual Recipe\n"
+      "2. Meal Shuffle!\n")
+active = True
+while active:
+    recipe_prompt = input("Please input the number 1 for an individual recipe, 2 for meal shuffle, or 'q' to quit: ")
+    if recipe_prompt == 'q':
+        active = False
+    elif recipe_prompt == '1':
+        displayData()
+        pullRecipe()
+    elif recipe_prompt == '2':
+        randomizeRecipe()
+
